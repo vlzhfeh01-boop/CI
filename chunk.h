a5ba4464aed8c61ed83f323e3208829d14f85c8d
Junki <vlzhfeh01@gmail.com>
2026-07-16 15:46:28 +0900

    Ch14 study

--- chunk.h
+++ chunk.h
@@ -2,8 +2,10 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum{
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -11,10 +13,12 @@ typedef struct{
     int count; // number of elements in the array we have allocated.
     int capacity; // how many of those allocated entries are actually in use.
     uint8_t* code; // pointer, array
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
-
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 #endif
--- main.c
+++ main.c
@@ -5,7 +5,12 @@
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk,OP_RETURN);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT,123); // OP_CONSTANT뒤에는 자동으로 숫자가 와야 한다.
+    writeChunk(&chunk, constant,123);
+
+    writeChunk(&chunk,OP_RETURN,123);
 
     disassembleChunk(&chunk,"test chunk");
     freeChunk(&chunk);
